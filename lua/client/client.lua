8d6870e50f69ad62bed17a3d4c45025032bb730f
itsruin <profit3027@gmail.com>
2024-01-14 00:18:54 +0900

    [/] 트리거 이벤트명 수정, 주석 제거

--- lua/client/client.lua
+++ lua/client/client.lua
@@ -6,13 +6,13 @@ local discordRichPresenceNameS = ""
 
 Citizen.CreateThread(function()
     while true do
-        TriggerServerEvent('h_discordActive:server:vRPsyncLoop')
+        TriggerServerEvent('itsruin_discordActive/server/vRPsyncLoop')
 		Wait(5 * 60 * 1000)
 	end
 end)
 
-RegisterNetEvent('h_discordActive:client:vRPsync')
-AddEventHandler('h_discordActive:client:vRPsync', function(name, user_id, faction)
+RegisterNetEvent('itsruin_discordActive/client/vRPsync')
+AddEventHandler('itsruin_discordActive/client/vRPsync', function(name, user_id, faction)
     local vRPsync = ""..name.. " ("..user_id.."번 · "..faction..")"
 
     Citizen.CreateThread(function()
@@ -41,31 +41,22 @@ AddEventHandler('h_discordActive:client:vRPsync', function(name, user_id, factio
                 elseif GetVehiclePedIsUsing(PlayerPedId()) ~= nil and not IsPedInAnyHeli(PlayerPedId()) and not IsPedInAnyPlane(PlayerPedId()) and not IsPedOnFoot(PlayerPedId()) and not IsPedInAnySub(PlayerPedId()) and not IsPedInAnyBoat(PlayerPedId()) then
     		    	local vehicleSpeed = GetEntitySpeed(GetVehiclePedIsUsing(PlayerPedId()))
     		    	local curSpeed = math.ceil(vehicleSpeed * 3.6) or math.ceil(vehicleSpeed * 2.236936)
-    		    	-- local vehicleName = GetLabelText(GetDisplayNameFromVehicleModel(GetEntityModel(GetVehiclePedIsUsing(PlayerPedId()))))
     		    	if curSpeed > 150 then
-    		    		-- SetRichPresence("".. vehicleName .."을 타고".. streetName .."에서 과속 운전 중\n".. vRPsync .."")
                     SetRichPresence("".. streetName .."에서 과속 운전하는 중 (규정속도 초과)\n".. vRPsync .."")
     		    	elseif curSpeed <= 150 and curSpeed > 0 then
-    		    		-- SetRichPresence("".. vehicleName .."을 타고".. streetName .."에서 안전 운전 중\n".. vRPsync .."")
                         SetRichPresence("".. streetName .."에서 안전 운전하는 중 (규정속도 이하)\n".. vRPsync .."")
     		    	elseif curSpeed == 0 then
-                        -- SetRichPresence("".. vehicleName .."을 타고".. streetName .."에서 정차해 있는 중\n".. vRPsync .."")
     		    		SetRichPresence(""..streetName.."에 정차해 있는 중\n".. vRPsync .."")
     		    	end
                 elseif IsPedInAnyHeli(PlayerPedId()) or IsPedInAnyPlane(PlayerPedId()) then
-    		    	-- local vehicleName = GetLabelText(GetDisplayNameFromVehicleModel(GetEntityModel(GetVehiclePedIsUsing(PlayerPedId()))))
     		    	if IsEntityInAir(GetVehiclePedIsUsing(PlayerPedId())) or GetEntityHeightAboveGround(GetVehiclePedIsUsing(PlayerPedId())) > 5.0 then
-                        -- SetRichPresence("".. vehicleName .."을 타고".. streetName .." 상공에 있는 중\n".. vRPsync .."")
                         SetRichPresence("".. streetName .." 상공에 있는 중 (비행기/헬기 탑승 중)\n".. vRPsync .."")
     		    	else
-                        -- SetRichPresence("".. vehicleName .."을 타고".. streetName .." 지상에 있는 중\n".. vRPsync .."")
                         SetRichPresence("".. streetName .." 지상에 있는 중 (비행기/헬기 탑승 중)\n".. vRPsync .."")
     		    	end
                 elseif IsEntityInWater(PlayerPedId()) then
     		    	SetRichPresence("어딘가에서 수영하는 중\n".. vRPsync .."")
     		    elseif IsPedInAnyBoat(PlayerPedId()) and IsEntityInWater(GetVehiclePedIsUsing(PlayerPedId())) then
-    		    	-- local vehicleName = GetLabelText(GetDisplayNameFromVehicleModel(GetEntityModel(GetVehiclePedIsUsing(PlayerPedId()))))
-    		    	-- SetRichPresence("".. vehicleName .."를 타고 항해 중 (보트/선박 탐승 중)\n".. vRPsync .."")
                     SetRichPresence("바다에서 항해 중 (보트/선박 탑승 중)\n".. vRPsync .."")
     		    elseif IsPedInAnySub(PlayerPedId()) and IsEntityInWater(GetVehiclePedIsUsing(PlayerPedId())) then
     		    	SetRichPresence("바다에서 잠수 중\n".. vRPsync .."")
